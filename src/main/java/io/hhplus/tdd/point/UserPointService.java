08f54bbbc1f6d39ec481d6dbb5a98eecef22f112
jaeseung choun <jaeseung_choun@tmax.co.kr>
2024-12-18 20:14:10 +0900

    Feat: 포인트 충전 기능

--- src/main/java/io/hhplus/tdd/point/UserPointService.java
+++ src/main/java/io/hhplus/tdd/point/UserPointService.java
@@ -8,6 +8,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserPointService {
   private final UserPointTable userPointTable;
-  public void chargePointById(Long id, Long chargeAmount) {};
+  public void chargePointById(Long id, Long chargeAmount) throws Exception {
+    UserPoint userPoint = userPointTable.selectById(id);
+    Long currentBalance = userPoint.point();
+    if(currentBalance + chargeAmount > 1_000_000L) {
+      throw new Exception("최대 잔고 초과");
+    }
+  };
   public void usePointById(Long id, Long point) {};
 }
