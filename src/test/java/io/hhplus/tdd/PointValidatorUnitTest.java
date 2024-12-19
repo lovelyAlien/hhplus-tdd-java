bff1c6c37da10f146956f904fd54e3bbf9b37a1c
jaeseung choun <jaeseung_choun@tmax.co.kr>
2024-12-19 18:19:46 +0900

    Test: 포인트 검증 단위 테스트

--- src/test/java/io/hhplus/tdd/PointValidatorUnitTest.java
+++ src/test/java/io/hhplus/tdd/PointValidatorUnitTest.java
@@ -1,2 +1,32 @@
-package io.hhplus.tdd;public class PointValidatorUnitTest {
+package io.hhplus.tdd;
+
+import io.hhplus.tdd.point.PointValidator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+public class PointValidatorUnitTest {
+
+  private PointValidator pointValidator;
+
+  @Test
+  void 사용_또는_충전_포인트_NULL() {
+    // given
+    Long amount = 1L;
+    assertThrows(IllegalArgumentException.class, () -> {
+      pointValidator.validateAmount(amount);
+    });
+  }
+
+  @Test
+  void 사용_또는_충전_포인트_음수() {
+    // given
+    Long amount = -100_000L;
+    assertThrows(IllegalArgumentException.class, () -> {
+      pointValidator.validateAmount(amount);
+    });
+  }
 }
